04fab904ee98fd735f0e42a1854948fd4d870fca
Fabrice G <fabou291@gmail.com>
2023-03-18 09:05:05 +0100

    💄 Implement index.css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './assets/css/index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <div>Hello</div>
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
