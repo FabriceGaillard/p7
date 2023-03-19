da85c650e11d80f629b44ce08dc4f298ccf142b2
Fabrice G <fabou291@gmail.com>
2023-03-19 13:25:13 +0100

    ✨ create about page

---
+++ src/pages/About/About.js
@@ -0,0 +1,43 @@
+import styles from './About.module.css';
+import Collapse from '../../components/Collapse/Collapse';
+
+export default function About() {
+  return (
+    <>
+      <div className={styles['about-hero']} />
+      <section className={styles['about-section']}>
+        <Collapse summary="Fiabilité" opened>
+          <p>
+            Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+            photos sont conformes aux logements, et toutes les informations sont
+            régulièrement vérifiées par nos équipes.
+          </p>
+        </Collapse>
+        <Collapse summary="Respect" opened>
+          <p>
+            La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+            comportement discriminatoire ou de perturbation du voisinage
+            entraînera une exclusion de notre plateforme.
+          </p>
+        </Collapse>
+        <Collapse summary="Service" opened>
+          <p>
+            Nos équipes se tiennent à votre disposition pour vous fournir une
+            expérience parfaite. N&apos;hésitez pas à nous contacter si vous
+            avez la moindre question.
+          </p>
+        </Collapse>
+        <Collapse summary="Sécurité" opened>
+          <p>
+            La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que
+            pour les voyageurs, chaque logement correspond aux critères de
+            sécurité établis par nos services. En laissant une note aussi bien à
+            l&apos;hôte qu&apos;au locataire, cela permet à nos équipes de
+            vérifier que les standards sont bien respectés. Nous organisons
+            également des ateliers sur la sécurité domestique pour nos hôtes.
+          </p>
+        </Collapse>
+      </section>
+    </>
+  );
+}
